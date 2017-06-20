cac09fdaf484cf5e45599df8746c2c1386a5361c
Stanislav Synko <safitudo@gmail.com>
2017-06-19 19:28:08 -0700

    postinstall

---
+++ postinstall.sh
@@ -0,0 +1,5 @@
+if ! [ -f ../../spv.ldb.zip ]; then
+  curl -o ../../spv.ldb.zip https://developer.airbitz.co/download/spvdemo.ldb.zip
+  mkdir -p ../../db
+  unzip ../../spv.ldb.zip ../../db/spvdemo
+fi
