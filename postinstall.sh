d8a2561dfff8bb8c3dbfbdf764bce186149404ff
Stanislav Synko <safitudo@gmail.com>
2017-06-19 19:38:12 -0700

    keys fix

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
+mkdir -p ../../db
 if ! [ -f ../../spv.ldb.zip ]; then
-  curl -o ../../spv.ldb.zip https://developer.airbitz.co/download/spvdemo.ldb.zip
-  mkdir -p ../../db
+  curl -o ../../spv.ldb.zip https://developer.airbitz.co/download/spvdemo.ldb.zip  
   unzip ../../spv.ldb.zip -d ../../db
 fi
