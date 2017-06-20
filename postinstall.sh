7cdd82dffa853c467dec9ad11e37bfe52b6ee4d6
Stanislav Synko <safitudo@gmail.com>
2017-06-19 19:33:05 -0700

    postinstall fix correct path

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 if ! [ -f ../../spv.ldb.zip ]; then
   curl -o ../../spv.ldb.zip https://developer.airbitz.co/download/spvdemo.ldb.zip
   mkdir -p ../../db
-  unzip ../../spv.ldb.zip -d ../../db/spvdemo
+  unzip ../../spv.ldb.zip -d ../../db
 fi
