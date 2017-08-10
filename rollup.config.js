0fb603df648b702fa5e1058a15f959750aa0878d
thehobbit85 <thehobbit85@gmail.com>
2017-08-10 16:18:30 -0700

    working build script for nodejs

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import buble from 'rollup-plugin-buble'
+import babel from 'rollup-plugin-babel'
 const packageJson = require('./package.json')
 
 export default {
   entry: 'src/index.js',
   external: Object.keys(packageJson.dependencies),
-  plugins: [buble()],
+  plugins: [babel({})],
   targets: [
     {
       dest: packageJson['main'],
