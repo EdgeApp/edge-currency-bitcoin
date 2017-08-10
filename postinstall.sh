356736b5badf3928078ea9fe44d95920072cf3e6
thehobbit85 <thehobbit85@gmail.com>
2017-08-10 14:50:53 -0700

    cleanup

--- postinstall.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../../db
-if ! [ -f ../../spv.ldb.zip ]; then
-  curl -o ../../spv.ldb.zip https://developer.airbitz.co/download/spvdemo.ldb.zip  
-  unzip ../../spv.ldb.zip -d ../../db
-fi
