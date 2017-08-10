097a3da58f5f6cec2d2b91776216656c29103227
thehobbit85 <thehobbit85@gmail.com>
2017-08-10 16:19:42 -0700

    tests

--- test/libTests.js
+++ test/libTests.js
@@ -1,40 +1,43 @@
 /* global describe it */
-const { makeBitcoinPlugin } = require('../lib/index.js')
-const assert = require('assert')
-
-function fakeRandom () {
-
-}
-const plugin = makeBitcoinPlugin({io: {random: fakeRandom}})
+let BitcoinPlugin = require('../lib/index.js').BitcoinPlugin
+let assert = require('assert')
 
 describe('parseUri', function () {
+  let plugin
+
+  before('Plugin', function () {
+    BitcoinPlugin.makePlugin({io: {fetch: () => true}}).then((bitcoinPlugin) => {
+      assert.equal(bitcoinPlugin.currencyInfo.currencyCode, 'BTC')
+      plugin = bitcoinPlugin
+    })
+  })
   it('address only', function () {
-    const parsedUri = plugin.parseUri('1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
+    let parsedUri = plugin.parseUri('1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, null)
     assert.equal(parsedUri.currencyCode, null)
   })
   it('uri address', function () {
-    const parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
+    let parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, null)
     assert.equal(parsedUri.currencyCode, null)
   })
   it('uri address with amount', function () {
-    const parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=12345.6789')
+    let parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=12345.6789')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, '1234567890000')
     assert.equal(parsedUri.currencyCode, 'BTC')
   })
   it('uri address with amount & label', function () {
-    const parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.56789&label=Johnny%20Bitcoin')
+    let parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.56789&label=Johnny%20Bitcoin')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, '123456789000')
     assert.equal(parsedUri.currencyCode, 'BTC')
     assert.equal(parsedUri.label, 'Johnny Bitcoin')
   })
   it('uri address with amount, label & message', function () {
-    const parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.56789&label=Johnny%20Bitcoin&message=Hello%20World,%20I%20miss%20you%20!')
+    let parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.56789&label=Johnny%20Bitcoin&message=Hello%20World,%20I%20miss%20you%20!')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, '123456789000')
     assert.equal(parsedUri.currencyCode, 'BTC')
@@ -42,7 +45,7 @@ describe('parseUri', function () {
     assert.equal(parsedUri.message, 'Hello World, I miss you !')
   })
   it('uri address with unsupported param', function () {
-    const parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?unsupported=helloworld&amount=12345.6789')
+    let parsedUri = plugin.parseUri('bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?unsupported=helloworld&amount=12345.6789')
     assert.equal(parsedUri.publicAddress, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
     assert.equal(parsedUri.nativeAmount, '1234567890000')
     assert.equal(parsedUri.currencyCode, 'BTC')
@@ -50,12 +53,19 @@ describe('parseUri', function () {
 })
 
 describe('encodeUri', function () {
+  let plugin
+
+  before('Plugin', function () {
+    BitcoinPlugin.makePlugin({io: {fetch: () => true}}).then((bitcoinPlugin) => {
+      plugin = bitcoinPlugin
+    })
+  })
   it('address only', function () {
-    const encodedUri = plugin.encodeUri({publicAddress: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'})
+    let encodedUri = plugin.encodeUri({publicAddress: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'})
     assert.equal(encodedUri, '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
   })
   it('address & amount', function () {
-    const encodedUri = plugin.encodeUri(
+    let encodedUri = plugin.encodeUri(
       {
         publicAddress: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX',
         nativeAmount: '123456780000'
@@ -64,7 +74,7 @@ describe('encodeUri', function () {
     assert.equal(encodedUri, 'bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.5678')
   })
   it('address, amount, and label', function () {
-    const encodedUri = plugin.encodeUri(
+    let encodedUri = plugin.encodeUri(
       {
         publicAddress: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX',
         nativeAmount: '123456780000',
@@ -75,7 +85,7 @@ describe('encodeUri', function () {
     assert.equal(encodedUri, 'bitcoin:1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX?amount=1234.5678&label=Johnny%20Bitcoin')
   })
   it('address, amount, label, & message', function () {
-    const encodedUri = plugin.encodeUri(
+    let encodedUri = plugin.encodeUri(
       {
         publicAddress: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX',
         nativeAmount: '123456780000',
