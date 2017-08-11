28802c5403dda1a321d0b53bc8ca811e5b601acb
thehobbit85 <thehobbit85@gmail.com>
2017-08-11 14:07:54 -0700

    more tests added

--- test/libTests.js
+++ test/libTests.js
@@ -1,12 +1,116 @@
 /* global describe it */
 let BitcoinPlugin = require('../lib/index.js').BitcoinPlugin
 let assert = require('assert')
+const { bns } = require('biggystring')
+
+let opts = {
+  io: {
+    fetch: () => true,
+    random: (size) => Array(size).fill(0).map((x, i) => i)
+  }
+}
+// describe('Plugin', function () {
+//   it('Get currency info', function () {
+//     BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
+//       assert.equal(bitcoinPlugin.currencyInfo.currencyCode, 'BTC')
+//     })
+//   })
+//   it('Get block height', function () {
+//     BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
+//       const walletInfoPrivate = bitcoinPlugin.createPrivateKey('wallet:bitcoin')
+//       const walletInfoPublic = bitcoinPlugin.derivePublicKey(walletInfoPrivate)
+//       const keys = Object.assign({}, walletInfoPrivate.keys, walletInfoPublic.keys)
+//       const walletInfo = walletInfoPublic
+//       walletInfo.keys = keys
+//       const engine = bitcoinPlugin.makeEngine(walletInfo)
+
+//       engine.startEngine()
+//       const height = engine.getBlockHeight()
+//       const success = (height === '0' || bns.gt(height, '100000'))
+
+//       assert.equal(success, true)
+//     })
+//   })
+// })
+
+describe('Info', function () {
+  let plugin
+
+  before('Plugin', function () {
+    BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
+      plugin = bitcoinPlugin
+    })
+  })
+
+  it('Test Currency code', function () {
+    assert.equal(plugin.currencyInfo.currencyCode, 'BTC')
+  })
+})
+
+describe('createPrivateKey', function () {
+  let plugin
+
+  before('Plugin', function () {
+    BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
+      plugin = bitcoinPlugin
+    })
+  })
+
+  it('Test Currency code', function () {
+    assert.equal(plugin.currencyInfo.currencyCode, 'BTC')
+  })
+
+  it('Create valid key', function () {
+    let walletInfo = plugin.createPrivateKey('wallet:bitcoin')
+    assert.equal(!walletInfo.keys, false)
+    assert.equal(typeof walletInfo.keys.bitcoinKey, 'string')
+    var a = Buffer.from(walletInfo.keys.bitcoinKey, 'base64')
+    var b = a.toString('hex')
+    assert.equal(b, '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f')
+  })
+})
+
+describe('derivePublicKey', function () {
+  let plugin
+
+  before('Plugin', function () {
+    BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
+      assert.equal(bitcoinPlugin.currencyInfo.currencyCode, 'BTC')
+      plugin = bitcoinPlugin
+    })
+  })
+  it('Valid private key', function () {
+    const walletInfo = plugin.derivePublicKey({
+      type: 'bitcoin',
+      keys: {'bitcoinKey': '12345678abcd'}
+    })
+    assert.equal(walletInfo.keys.masterPublicKey.toLowerCase(), 'pub12345678abcd'.toLowerCase())
+  })
+
+  it('Invalid key name', function () {
+    assert.throws(() => {
+      plugin.derivePublicKey({
+        type: 'bitcoin',
+        keys: {'bitcoinKeyz': '12345678abcd'}
+      })
+    })
+  })
+
+  it('Invalid wallet type', function () {
+    assert.throws(() => {
+      plugin.derivePublicKey({
+        type: 'shitcoin',
+        keys: {'bitcoinKey': '12345678abcd'}
+      })
+    })
+  })
+})
 
 describe('parseUri', function () {
   let plugin
 
   before('Plugin', function () {
-    BitcoinPlugin.makePlugin({io: {fetch: () => true}}).then((bitcoinPlugin) => {
+    BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
       assert.equal(bitcoinPlugin.currencyInfo.currencyCode, 'BTC')
       plugin = bitcoinPlugin
     })
@@ -56,7 +160,7 @@ describe('encodeUri', function () {
   let plugin
 
   before('Plugin', function () {
-    BitcoinPlugin.makePlugin({io: {fetch: () => true}}).then((bitcoinPlugin) => {
+    BitcoinPlugin.makePlugin(opts).then((bitcoinPlugin) => {
       plugin = bitcoinPlugin
     })
   })
