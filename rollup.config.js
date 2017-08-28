04adc34f9594e139badfd7c7d8abb97ba4e788f8
thehobbit85 <thehobbit85@gmail.com>
2017-08-28 00:08:56 -0700

    inject correct requires when building for react-native

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+
 const packageJson = require('./package.json')
 
 export default {
   entry: 'src/index.js',
   external: Object.keys(packageJson.dependencies),
-  plugins: [babel({})],
+  plugins: [
+    replace({
+      'import bcoin from \'bcoin\'': 'let bcoin = require(\'../vendor/bcoin.js\') \n let Buffer = require(\'buffer/\').Buffer\n'
+    }),
+    babel({})
+  ],
   targets: [
     {
       dest: packageJson['main'],
