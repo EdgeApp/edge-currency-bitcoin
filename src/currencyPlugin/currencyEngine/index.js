a55531e2e162841c2526cd9f6dda22f18b87421d
thehobbit85 <thehobbit85@gmail.com>
2017-09-14 00:42:07 -0700

    removed log

--- src/currencyPlugin/currencyEngine/index.js
+++ src/currencyPlugin/currencyEngine/index.js
@@ -165,7 +165,6 @@ export default (bcoin, txLibInfo) => class CurrencyEngine {
       //   }
       // }
     }
-    console.log(this.walletLocalData.addresses)
   }
   /* --------------------------------------------------------------------- */
   /* ---------------------------  Public API  ---------------------------- */
