a9205e561b81b910f35857900d16d75494b210b5
thehobbit85 <thehobbit85@gmail.com>
2017-09-26 23:29:37 -0700

    moved bitcoin cash code to a new bcoin fork

--- src/currencyInfo/index.js
+++ src/currencyInfo/index.js
@@ -1,4 +1,4 @@
 export * from './bitcoin.js'
-// export * from './bitcoincash.js'
+export * from './bitcoincash.js'
 export * from './litecoin.js'
 // export * from './dogecoin.js'
--- src/currencyPlugin/bcoin.js
+++ src/currencyPlugin/bcoin.js
@@ -1,22 +1,10 @@
-const SIGHASH_FORKID = 0x40
-const SIGHASH_ALL = 0x01
-const SEGWIT_VER = 1
-
-// Patch bcoin with bitcoincash compatibility:
-// 1. Flip the FORKID bit in the sighash type
-// 2. Always use segwit's sighash digest algorithm (BIP 143)
-const bitcoinCashPatch = (bcoin, proto = bcoin.primitives.TX.prototype, fn = proto.signature) =>
-  (proto.signature = function (index, prev, value, key, type = SIGHASH_ALL, version) {
-    return fn.call(this, index, prev, value, key, type | SIGHASH_FORKID, SEGWIT_VER)
-  }) && bcoin
-
 export default (txLibInfo) => {
   const currencyName = txLibInfo.getInfo.currencyName.toLowerCase()
   let bcoin
   if (currencyName === 'bitcoin') {
     bcoin = require('bcoin')
   } else if (currencyName === 'bitcoincash') {
-    bcoin = bitcoinCashPatch(require('bcoin'))
+    bcoin = require('bccoin')
   } else {
     bcoin = require('lcoin')
   }
