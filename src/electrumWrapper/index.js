bec538bbf197df326715c51f0dc68ad907f038b4
thehobbit85 <thehobbit85@gmail.com>
2017-10-04 16:41:21 -0700

    optimized connection distribuition and created disconnection handeling

--- src/electrumWrapper/index.js
+++ src/electrumWrapper/index.js
@@ -1,56 +1,55 @@
 // @flow
 
-const MAX_CONNECTIONS = 4
-const MAX_REQUEST_TIME = 1000
-const MAX_CONNECTION_HANG_TIME = 2500
-
-// Replacing net module for ReactNative
-let getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-let randomHash = () => 'a' + Math.random().toString(36).substring(7)
-
 export class Electrum {
-  globalRecievedData: Array<any>
+  globalRecievedData: Array<string>
   connected: boolean
   io: any
+  currentConn: number
   connections: Array<any>
-  requests: any
+  requests: {
+    data: string,
+    connectionIndex: number,
+    onDataReceived: any,
+    onFailure: any
+  }
+  id: number
   serverList: Array<Array<string>>
   subscribers: {
     address: any,
     numblocks: any
   }
   constructor (serverList: Array<Array<string>>, callbacks: any, io: any) {
-    let serverIndex = []
-    this.globalRecievedData = []
-
-    // Compiling serverList
-    while (serverIndex.length < MAX_CONNECTIONS && serverIndex.length < serverList.length) {
-      let j = getRandomInt(0, serverList.length - 1)
-      if (serverIndex.indexOf(serverList[j]) === -1) {
-        serverIndex.push(serverList[j])
-        this.globalRecievedData.push('')
-      }
-    }
     this.connected = false
     this.io = io
     this.connections = []
     this.requests = {}
-    this.serverList = serverIndex
+    this.serverList = serverList
+    this.globalRecievedData = Array(serverList.length).fill('')
     this.subscribers = {
       address: callbacks.onAddressStatusChanged,
       numblocks: callbacks.onBlockHeightChanged
     }
+    this.currentConn = 0
+    this.id = 0
+  }
+
+  getID (): number {
+    return this.id++
+  }
+  getNextConn (): number {
+    if (this.currentConn === this.serverList.length - 1) {
+      this.currentConn = 0
+    } else this.currentConn++
+    return this.currentConn
   }
 
   compileDataCallback (index: number) {
-    let this$1 = this
-
-    return function (data: any) {
+    return (data: any) => {
       let string = data.toString('utf8')
-      this$1.globalRecievedData[index] += string
+      this.globalRecievedData[index] += string
       let result = []
-      if (this$1.globalRecievedData[index].includes('\n')) {
-        let mdata = this$1.globalRecievedData[index].split('\n')
+      if (this.globalRecievedData[index].includes('\n')) {
+        let mdata = this.globalRecievedData[index].split('\n')
         for (var k = 0; k <= mdata.length - 1; k++) {
           if (mdata[k].length) {
             try {
@@ -61,34 +60,15 @@ export class Electrum {
             }
           }
         }
-        this$1.globalRecievedData[index] = mdata.slice(k, mdata.length).join('')
+        this.globalRecievedData[index] = mdata.slice(k, mdata.length).join('')
       } else {
         try {
-          const res = JSON.parse(this$1.globalRecievedData[index])
+          const res = JSON.parse(this.globalRecievedData[index])
           result.push(res)
-          this$1.globalRecievedData[index] = ''
+          this.globalRecievedData[index] = ''
         } catch (e) {}
       }
-      result.forEach(r => this$1.handleData(r))
-    }
-  }
-
-  collectGarbage () {
-    this.connections.forEach((connection, index) => {
-      if (connection.lastRequest - connection.lastResponse > MAX_CONNECTION_HANG_TIME) {
-        let callback = this.compileDataCallback(index)
-        return this.netConnect(this.serverList[index][1], this.serverList[index][0], callback, index)
-      }
-    })
-    let now = Date.now()
-    for (let i in this.requests) {
-      if (now - this.requests[i].requestTime > MAX_REQUEST_TIME && !this.requests[i].executed) {
-        this.requests[i].requestTime = now
-        let randomIndex = getRandomInt(0, Math.min(MAX_CONNECTIONS, this.connections.length) - 1)
-        if (this.socketWriteAbstract(randomIndex, this.requests[i].data) === 2) {
-          this.connections[randomIndex].lastRequest = now
-        }
-      }
+      result.forEach(r => this.handleData(r))
     }
   }
 
@@ -104,34 +84,27 @@ export class Electrum {
       connection._state = 2
       resolveProxy(1)
     })
-    // serverCache(connection)
+
     connection._state = 1
+
     let now = Date.now()
 
-    // Setting up callbacks
+    const gracefullyCloseConn = (e: any) => {
+      connection._state = 0
+      for (let id in this.requests) {
+        const request = this.requests[id]
+        if (request.connectionIndex === i) {
+          const { method, params } = JSON.parse(request.data)
+          this.write(method, params)
+          delete this.requests[id]
+        }
+      }
+    }
+
     connection.on('data', callback)
-
-    connection.on('close', e => {
-      connection._state = 0
-      // console.log("[] recieved close", e)
-    })
-
-    connection.on('connect', e => {
-      // console.log("[] recieved connect", e)
-    })
-
-    connection.on('error', e => {
-      // console.log("[] recieved error", e)
-    })
-
-    connection.on('onerror', e => {
-      // console.log("[] recieved close", e)
-    })
-
-    connection.on('end', e => {
-      connection._state = 0
-      // console.log("[] recieved end", e)
-    })
+    connection.on('close', gracefullyCloseConn)
+    connection.on('error', gracefullyCloseConn)
+    connection.on('end', gracefullyCloseConn)
 
     let conn = {
       conn: connection,
@@ -147,30 +120,26 @@ export class Electrum {
   }
 
   connect () {
-    this.serverList.forEach((server, index) => {
+    this.serverList.forEach((server: Array<string>, index: number) => {
       let callback = this.compileDataCallback(index)
       this.netConnect(server[1], server[0], callback, -1)
     })
-    setInterval(() => {
-      this.collectGarbage()
-    }, 40000)
   }
 
   socketWriteAbstract (index: number, data: any) {
-    if (this.connections[index].conn._state === 0) {
-      var callback = this.compileDataCallback(index)
-      this.netConnect(this.serverList[index][1], this.serverList[index][0], callback, index)
-      return 0
-    }
-    if (this.connections[index].conn._state === 1) {
-      this.connections[index].prom.then(() => {
+    switch (this.connections[index].conn._state) {
+      case 0:
+        var callback = this.compileDataCallback(index)
+        this.netConnect(this.serverList[index][1], this.serverList[index][0], callback, index)
+        break
+      case 1:
+        this.connections[index].prom.then(() => {
+          this.connections[index].conn.write(data + '\n')
+        })
+        break
+      case 2:
         this.connections[index].conn.write(data + '\n')
-      })
-      return 1
-    }
-    if (this.connections[index].conn._state === 2) {
-      this.connections[index].conn.write(data + '\n')
-      return 2
+        break
     }
   }
 
@@ -179,44 +148,31 @@ export class Electrum {
       const method = data.method.split('.')
       if (method.length === 3 && method[2] === 'subscribe') {
         this.subscribers[method[1]](...data.params)
-        return
       }
+    } else if (typeof this.requests[data.id] === 'object') {
+      this.requests[data.id].onDataReceived(data.result)
+      delete this.requests[data.id]
     }
-    if (typeof this.requests[data.id] !== 'object') return
-    if (this.requests[data.id].executed) return
-    var now = Date.now()
-    this.connections[this.requests[data.id].connectionIndex].lastResponse = now
-    this.requests[data.id].executed = 1
-    this.requests[data.id].onDataReceived(data.result)
   }
 
-  write (method: string, params: Array<any>) {
+  write (method: string, params: Array<any>): Promise<any> {
     let rejectProxy, resolveProxy
-    const hash = randomHash()
-    const randomIndex = getRandomInt(0, Math.min(MAX_CONNECTIONS, this.connections.length) - 1)
-    let data = JSON.stringify({
-      id: hash,
-      method: method,
-      params: params
-    })
+    const id = this.getID().toString()
+    const nextConnection = this.getNextConn()
+    const data = JSON.stringify({ id, method, params })
+
     const out = new Promise((resolve, reject) => {
       resolveProxy = resolve
       rejectProxy = resolve
     })
 
-    const now = Date.now()
-
-    this.requests[hash] = {
+    this.requests[id] = {
       data: data,
-      executed: 0,
-      connectionIndex: randomIndex,
+      connectionIndex: nextConnection,
       onDataReceived: resolveProxy,
-      onFailure: rejectProxy,
-      requestTime: now
+      onFailure: rejectProxy
     }
-    this.connections[randomIndex].lastRequest = now
-    this.socketWriteAbstract(randomIndex, data)
-
+    this.socketWriteAbstract(nextConnection, data)
     return out
   }
 
