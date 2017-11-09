e9180954bf00d5c6fd4fd230ba21ff5dbb44511f
Paul Puey <paul@airbitz.co>
2017-11-08 18:27:31 -0800

    Remove mistaken return command
    Bump version

--- src/currencyEngine/indexEngine.js
+++ src/currencyEngine/indexEngine.js
@@ -317,7 +317,6 @@ export default (bcoin:any, txLibInfo:any) => class CurrencyEngine implements Abc
 
   async startEngine () {
     console.log('Skipping startEngine')
-    return
     this.wallet.on('balance', balance => {
       const confirmedBalance = balance.confirmed.toString()
       const unconfirmedBalance = balance.unconfirmed.toString()
