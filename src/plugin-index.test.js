163ea31cc8cef6c90a220b8caf294c96213ef265
William Swanson <swansontec@gmail.com>
2017-11-17 13:40:11 -0800

    Add a unit-test for the context

---
+++ src/plugin-index.test.js
@@ -0,0 +1,50 @@
+// @flow
+import { expect } from 'chai'
+import { describe, it, afterEach } from 'mocha'
+import {
+  makeFakeContexts,
+  makeFakeIos,
+  destroyAllContexts
+} from 'airbitz-core-js'
+import type { AbcAccount } from 'airbitz-core-types'
+
+import {
+  BitcoinPluginFactory,
+  BitcoincashPluginFactory,
+  DogecoinPluginFactory,
+  LitecoinPluginFactory
+} from './plugin-index.js'
+
+const plugins = [
+  BitcoinPluginFactory,
+  BitcoincashPluginFactory,
+  DogecoinPluginFactory,
+  LitecoinPluginFactory
+]
+
+async function makeFakeAccount (plugins): Promise<AbcAccount> {
+  const [context] = makeFakeContexts({ plugins })
+  return context.createAccount('fake user', void 0, '1111', {})
+}
+
+afterEach(function () {
+  destroyAllContexts()
+})
+
+for (const pluginFactory of plugins) {
+  const { pluginName } = pluginFactory
+
+  describe(`${pluginName} plugin`, function () {
+    it('can be created manually', async function () {
+      const [io] = makeFakeIos()
+      expect(pluginFactory.pluginType).to.equal('currency')
+      const currencyPlugin = await pluginFactory.makePlugin({ io })
+      expect(currencyPlugin.currencyInfo.pluginName).to.equal(pluginName)
+    })
+
+    it('can be created by the core', async function () {
+      const account = await makeFakeAccount([pluginFactory])
+      account.logout()
+    })
+  })
+}
