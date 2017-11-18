e343dc994740629b248b198152a15b8b1054ef27
thehobbit85 <thehobbit85@gmail.com>
2017-11-18 02:38:09 -0800

    Adding all plugins and bcoin-extender to main
    Side note, the way this file looks now is horrible. every thing is refered 3 times.
    Needs to be better written for future expendability

--- src/plugin-index.js
+++ src/plugin-index.js
@@ -7,10 +7,30 @@ import type {
 } from 'airbitz-core-types'
 
 import { bitcoinInfo } from './info/bitcoin.js'
+import { bitcoinTestnetInfo } from './info/bitcointestnet.js'
 import { bitcoincashInfo } from './info/bitcoincash.js'
+import { bitcoincashTestnetInfo } from './info/bitcoincashtestnet.js'
 import { dogecoinInfo } from './info/dogecoin.js'
+import { dogecoinTestnetInfo } from './info/dogecointestnet.js'
 import { litecoinInfo } from './info/litecoin.js'
+import { litecoinTestnetInfo } from './info/litecointestnet.js'
 import { CurrencyPlugin } from './plugin/plugin-api.js'
+import { bcoinExtender } from './bcoin-extender/bcoin-extender'
+import bcoin from 'bcoin'
+
+const pluginsInfo = [
+  bitcoinInfo,
+  bitcoinTestnetInfo,
+  bitcoincashInfo,
+  bitcoincashTestnetInfo,
+  dogecoinInfo,
+  dogecoinTestnetInfo,
+  litecoinInfo,
+  litecoinTestnetInfo
+]
+
+// Extend bcoin to support all networks inside info
+bcoinExtender(bcoin, pluginsInfo)
 
 /**
  * Makes a core plugin factory, given the currencyInfo for that coin.
@@ -29,6 +49,10 @@ function makePluginFactory (
 }
 
 export const BitcoinPluginFactory = makePluginFactory(bitcoinInfo)
+export const BitcoinTestnetPluginFactory = makePluginFactory(bitcoinTestnetInfo)
 export const BitcoincashPluginFactory = makePluginFactory(bitcoincashInfo)
+export const BitcoincashTestnetPluginFactory = makePluginFactory(bitcoincashTestnetInfo)
 export const DogecoinPluginFactory = makePluginFactory(dogecoinInfo)
+export const DogecoinTestnetPluginFactory = makePluginFactory(dogecoinTestnetInfo)
 export const LitecoinPluginFactory = makePluginFactory(litecoinInfo)
+export const LitecoinTestnetPluginFactory = makePluginFactory(litecoinTestnetInfo)
