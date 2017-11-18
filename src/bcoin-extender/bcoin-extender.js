73e5f0c5c23f57f3356beb772b78da0870175ad1
thehobbit85 <thehobbit85@gmail.com>
2017-11-18 03:44:12 -0800

    fixed error and now the plugin works 100% with full support for different networks (bitcoin, litecoin, bitcoincash, testnet, etc...)

--- src/bcoin-extender/bcoin-extender.js
+++ src/bcoin-extender/bcoin-extender.js
@@ -7,6 +7,11 @@ export const bcoinExtender = (bcoin: any, pluginsInfo: Array<AbcCurrencyInfo>) =
   for (const pluginInfo: AbcCurrencyInfo of pluginsInfo) {
     const type = pluginInfo.defaultSettings.network.type
     bcoin.networks.types.push(type)
+    for (const param in bcoin.networks.main) {
+      if (!pluginInfo.defaultSettings.network[param]) {
+        pluginInfo.defaultSettings.network[param] = bcoin.networks.main[param]
+      }
+    }
     bcoin.networks[type] = pluginInfo.defaultSettings.network
   }
 }
