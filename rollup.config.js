4173f776c495acf043efeefdfe0c305a2578f357
William Swanson <swansontec@gmail.com>
2017-11-17 23:05:05 -0800

    Hack buffer in the build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,24 @@
+import alias from 'rollup-plugin-alias'
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
-import packageJson from './package.json'
-import replace from 'rollup-plugin-replace'
 
-const babelConf = {
+import packageJson from './package.json'
+
+const babelOptions = {
   presets: ['flow']
 }
 
 export default {
   external: [
     ...Object.keys(packageJson.dependencies),
-    ...Object.keys(packageJson.devDependencies)
+    ...Object.keys(packageJson.devDependencies),
+    'buffer/'
   ],
   input: './src/plugin-index.js',
   output: [
     { file: packageJson.main, format: 'cjs' },
     { file: packageJson.module, format: 'es' }
   ],
-  plugins: [
-    json(),
-    replace({
-      bufferPlaceHolder: 'buffer/'
-    }),
-    babel(babelConf)
-  ],
+  plugins: [json(), alias({ 'buffer-hack': 'buffer/' }), babel(babelOptions)],
   sourcemap: true
 }
--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,10 +1,23 @@
-import config from './rollup.config.js'
+import alias from 'rollup-plugin-alias'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
 import multiEntry from 'rollup-plugin-multi-entry'
+
+import config from './rollup.config.js'
+
+const babelOptions = {
+  presets: ['flow']
+}
 
 export default {
   external: config.external,
   input: 'src/**/*.test.js',
   output: [{ file: 'build/tests.cjs.js', format: 'cjs' }],
-  plugins: [multiEntry(), ...config.plugins],
+  plugins: [
+    multiEntry(),
+    json(),
+    alias({ 'buffer-hack': 'buffer' }),
+    babel(babelOptions)
+  ],
   sourcemap: true
 }
