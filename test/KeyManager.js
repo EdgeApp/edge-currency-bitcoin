b1dca8adb7e7c828cf415756a13cd6289e2dc58a
thehobbit85 <thehobbit85@gmail.com>
2017-11-20 13:19:36 -0800

    new test for key manager

---
+++ test/KeyManager.js
@@ -0,0 +1,41 @@
+// @flow
+
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+import disklet from 'disklet'
+import { KeyManager } from '../src/engine/keyManager.js'
+import { EngineState } from '../src/engine/engine-state.js'
+
+const walletInfo = {
+  id: '1',
+  type: 'wallet:bitcoin',
+  keys: {
+    bitcoinKey: 'chicken valve parrot park animal proof youth detail glance review artwork cluster drive more charge lunar uncle neglect brain act rose job photo spot',
+    bitcoinXpub: 'xpub661MyMwAqRbcF6JxG5NqmWiCbURzYtg95A5T7m6bdJ27FHDuLcVHmAg4unEMvdNi5VniUWgxxDJM5odBjUUzuSNCciED3sbfdX37NsdKTiQ'
+  }
+}
+
+const io = {
+  fetch: () => true,
+  random: (size) => size,
+  net: require('net')
+}
+
+const walletLocalFolder = disklet.makeMemoryFolder()
+
+const engineState = new EngineState({
+  callbacks: {},
+  bcoin: {}, // TODO: Implement this
+  io,
+  localFolder: walletLocalFolder
+})
+
+const gapLimit = 10
+
+describe(`Key Manager`, function () {
+  it('creates new key manager', function () {
+    const keyManager = new KeyManager(walletInfo, engineState, gapLimit)
+    assert.equal(keyManager.keys.receive.length, 10)
+    assert.equal(keyManager.keys.change.length, 0)
+  })
+})
