c9ce06e8e63b7fb7e8885bd5bdc362c4fa992457
Paul Puey <paul@airbitz.co>
2017-11-24 23:18:45 -0800

    Add subscribeHeight test

--- test/stratumTest.js
+++ test/stratumTest.js
@@ -6,7 +6,7 @@ import tls from 'tls'
 
 import type { StratumCallbacks } from '../src/stratum/stratum-connection.js'
 import { StratumConnection } from '../src/stratum/stratum-connection.js'
-import { fetchVersion } from '../src/stratum/stratum-messages.js'
+import { fetchVersion, subscribeHeight } from '../src/stratum/stratum-messages.js'
 
 const ELECTRUM_SERVER = 'electrum://electrum.villocq.com:50001'
 const io = {
@@ -20,6 +20,31 @@ describe('StratumConnection', function () {
       data => {
         const ver = parseFloat(data)
         expect(ver).to.be.at.least(1.1)
+        done()
+      },
+      () => {
+        throw new Error('should never happen')
+      }
+    )
+    let taskQueued = false
+    const callbacks: StratumCallbacks = {
+      onOpen (uri: string) {},
+      onClose (uri: string) {},
+      onQueueSpace (uri: string) {
+        if (taskQueued) {
+          return void 0
+        }
+        taskQueued = true
+        return task
+      }
+    }
+    const connection = new StratumConnection(ELECTRUM_SERVER, { callbacks, io })
+    connection.open()
+  })
+  it('subscribeHeight', function (done) {
+    const task = subscribeHeight(
+      data => {
+        expect(data).to.be.at.least(400000)
         done()
       },
       () => {
