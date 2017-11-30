38db5e4f3f4df9375fda3db6385071a2754fe991
thehobbit85 <thehobbit85@gmail.com>
2017-11-29 19:37:52 -0800

    patch bcoin for bitcoincash txs

--- src/utils/bcoin-extender.js
+++ src/utils/bcoin-extender.js
@@ -18,6 +18,21 @@ const patchBachAddress = bcoin => {
   }
 }
 
+const patchBcashTX = bcoin => {
+  const txProto = bcoin.primitives.Address.prototype
+  const signature = txProto.signature
+  txProto.signature = function (index, prev, value, key, type, version) {
+    if (type === -100) {
+      // Patch bcoin with bitcoincash compatibility:
+      // 1. Flip the FORKID bit in the sighash type
+      // 2. Always use segwit's sighash digest algorithm (BIP 143)
+      type = 0x01 | 0x40
+      version = 1
+    }
+    return signature.call(this, index, prev, value, key, type, version)
+  }
+}
+
 export const bcoinExtender = (
   bcoin: any,
   pluginsInfo: Array<AbcCurrencyInfo>
@@ -34,6 +49,7 @@ export const bcoinExtender = (
     bcoin.networks[type] = network
     if (!bcashPatch && type && type.includes('bitcoincash')) {
       patchBachAddress(bcoin)
+      patchBcashTX(bcoin)
       bcashPatch = true
     }
   }
