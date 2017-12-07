c7c5d48d76a12b0968f0442cde1e8e25ee9fef16
thehobbit85 <thehobbit85@gmail.com>
2017-12-06 17:57:50 -0800

    fix tests for keyManager which is now a full indepentend module.

--- test/KeyManager.js
+++ test/KeyManager.js
@@ -2,43 +2,36 @@
 
 import { describe, it } from 'mocha'
 import { assert } from 'chai'
-import disklet from 'disklet'
 import { KeyManager } from '../src/engine/keyManager.js'
-import { EngineState } from '../src/engine/engine-state.js'
+import type { KeyManagerCallbacks } from '../src/engine/keyManager.js'
 
 const network = 'bitcoin'
-const walletLocalFolder = disklet.makeMemoryFolder()
 const gapLimit = 10
 
-const walletInfo = {
-  id: '1',
-  type: 'wallet:bitcoin',
-  keys: {
-    bitcoinKey:
-      'chicken valve parrot park animal proof youth detail glance review artwork cluster drive more charge lunar uncle neglect brain act rose job photo spot',
-    bitcoinXpub:
-      'xpub661MyMwAqRbcF6JxG5NqmWiCbURzYtg95A5T7m6bdJ27FHDuLcVHmAg4unEMvdNi5VniUWgxxDJM5odBjUUzuSNCciED3sbfdX37NsdKTiQ'
+const keyManagerCallbacks: KeyManagerCallbacks = {
+  onNewAddress: (scriptHash: string, address: string, path: string) => {
+    console.log(scriptHash, address, path)
+  },
+  onNewKey: (keys: any) => {
+    console.log(keys)
   }
 }
-
-const io = {
-  fetch: () => true,
-  random: size => size,
-  net: require('net')
-}
-
-const engineState = new EngineState({
-  callbacks: {},
-  bcoin: {}, // TODO: Implement this
-  pluginState: ({}: any), // TODO: Implement this
-  io,
-  localFolder: walletLocalFolder
-})
 
 describe(`Key Manager`, function () {
   let keyManager
   it('creates new key manager', function () {
-    keyManager = new KeyManager(walletInfo, engineState, null, gapLimit, network)
+    keyManager = new KeyManager({
+      walletType: 'wallet:bitcoin',
+      rawKeys: {
+        master: {
+          xpub: 'xpub661MyMwAqRbcF6JxG5NqmWiCbURzYtg95A5T7m6bdJ27FHDuLcVHmAg4unEMvdNi5VniUWgxxDJM5odBjUUzuSNCciED3sbfdX37NsdKTiQ'
+        }
+      },
+      seed: 'chicken valve parrot park animal proof youth detail glance review artwork cluster drive more charge lunar uncle neglect brain act rose job photo spot',
+      gapLimit: gapLimit,
+      network: network,
+      callbacks: keyManagerCallbacks
+    })
     keyManager.load().then(() => {
       assert.equal(keyManager.keys.receive.children.length, 10)
       assert(keyManager.keys.receive.pubKey)
