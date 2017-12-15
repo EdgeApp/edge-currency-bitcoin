268cf3e85de861f85cc4507cc2f211bc293a6db2
William Swanson <swansontec@gmail.com>
2017-12-15 12:23:01 -0800

    Unit-test parsing the genesis transaction
    We are worried that coinbase transactions don't have normal inputs, so this unit test verifies that they have a proper txid and index, even if it's meaningles.

--- test/parseTransaction.js
+++ test/parseTransaction.js
@@ -37,4 +37,29 @@ describe('parseTransaction', function () {
 
     expect(parseTransaction(txData)).to.deep.equal(expected)
   })
+
+  it('Handles Bitcoin genesis transaction', function () {
+    // txid 4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b
+    const txData =
+      '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000'
+    const expected: ParsedTx = {
+      inputs: [
+        {
+          txid:
+            '0000000000000000000000000000000000000000000000000000000000000000',
+          index: 4294967295
+        }
+      ],
+      outputs: [
+        {
+          // displayAddress: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'
+          scriptHash:
+            '740485f380ff6379d11ef6fe7d7cdd68aea7f8bd0d953d9fdf3531fb7d531833',
+          value: 5000000000
+        }
+      ]
+    }
+
+    expect(parseTransaction(txData)).to.deep.equal(expected)
+  })
 })
