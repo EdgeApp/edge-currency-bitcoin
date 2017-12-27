53194affb2ddf9fbed55a2f688683300efd28557
thehobbit85 <thehobbit85@gmail.com>
2017-12-26 19:54:23 -0800

    move functions to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,7 @@
  */
 
 import { validate } from 'jsonschema'
+import crypto from 'crypto'
 
 export function validateObject (object: any, schema: any) {
   let result = null
@@ -15,4 +16,37 @@ export function validateObject (object: any, schema: any) {
   }
 
   return result && result.errors && result.errors.length === 0
+}
+
+export function sizeVarint (num: number) {
+  if (num < 0xfd) return 1
+  if (num <= 0xffff) return 3
+  if (num <= 0xffffffff) return 5
+  return 9
+}
+
+export async function hash256 (hex: any) {
+  return Promise.resolve(
+    crypto
+      .createHash('sha256')
+      .update(hex)
+      .digest()
+  )
+}
+
+export async function hash160 (hex: any) {
+  return Promise.resolve(
+    crypto
+      .createHash('ripemd160')
+      .update(await hash256(hex))
+      .digest()
+  )
+}
+
+export function reverseBufferToHex (rawBuffer: any) {
+  return rawBuffer
+    .toString('hex')
+    .match(/../g)
+    .reverse()
+    .join('')
 }
