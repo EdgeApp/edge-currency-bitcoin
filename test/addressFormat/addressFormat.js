58a563200d88dc2725dd8f57d9c1eea320ee7e36
thehobbit85 <thehobbit85@gmail.com>
2018-02-06 00:12:52 -0800

    put the addressFormat test into it's own folder

---
+++ test/addressFormat/addressFormat.js
@@ -0,0 +1,53 @@
+// @flow
+import { makeFakeIos } from 'edge-login'
+import { describe, it, before } from 'mocha'
+import * as Factories from '../../src/index.js'
+import { assert } from 'chai'
+import { toLegacyFormat } from '../../src/utils/addressFormat/addressFormatIndex.js'
+import fixtures from './fixtures.json'
+
+for (const fixture of fixtures) {
+  const CurrencyPluginFactory = Factories[fixture['factory']]
+  const WALLET_TYPE = fixture['WALLET_TYPE']
+
+  const [fakeIo] = makeFakeIos(1)
+  const opts = {
+    io: Object.assign(fakeIo, {
+      random: size => {},
+      Socket: require('net').Socket,
+      TLSSocket: require('tls').TLSSocket
+    })
+  }
+
+  describe(`Info for Wallet type ${WALLET_TYPE}`, function () {
+    let plugin
+
+    before('Plugin', function (done) {
+      CurrencyPluginFactory.makePlugin(opts).then(currencyPlugin => {
+        plugin = currencyPlugin
+        done()
+      })
+    })
+
+    it('test valid addresses', function () {
+      for (const address of fixture['valid']) {
+        plugin.encodeUri({ publicAddress: address })
+      }
+    })
+
+    it('test invalid addresses', function () {
+      for (const address of fixture['inValid']) {
+        assert.throws(() => {
+          plugin.encodeUri({ publicAddress: address })
+        }, 'InvalidPublicAddressError')
+      }
+    })
+
+    it('get legacy format', function () {
+      for (const address of fixture['toLegacy']) {
+        console.log(address)
+        assert(toLegacyFormat(address[0], fixture['WALLET_TYPE'].split('wallet:')[1]) === address[1])
+      }
+    })
+  })
+}
