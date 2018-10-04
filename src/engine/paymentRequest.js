9aaedb14508a64bc4e5612036798c482ac257ceb
Paul Puey <paul@edge.app>
2018-10-03 19:33:05 -0700

    Fix Bitpay payment requests on Android
    * Only use JSON payment request and remove BIP70 support
    * Use hacked in global.androidFetch if present which bypasses

--- src/engine/paymentRequest.js
+++ src/engine/paymentRequest.js
@@ -1,62 +1,9 @@
 // @flow
 // $FlowFixMe
-import buffer from 'buffer-hack'
-import { primitives, bip70 } from 'bcoin'
+import { primitives } from 'bcoin'
 import parse from 'url-parse'
 import type { EdgePaymentProtocolInfo } from 'edge-core-js'
 import { toNewFormat, toLegacyFormat } from '../utils/addressFormat/addressFormatIndex.js'
-
-const { Buffer } = buffer
-
-type FetchOptions = {
-  method: string,
-  headers: any,
-  body?: any
-}
-
-const fetchBuffer = async (
-  fetch: any,
-  url: string,
-  method: string,
-  headers: any,
-  data: any
-) => {
-  // Legacy fetching using XMLHttpRequest
-  // This is for enviroments that don't support 'arrayBuffer'
-  // like some versions of react-native and old browsers
-  const legacyBufferFetch = () =>
-    new Promise((resolve, reject) => {
-      const req = new window.XMLHttpRequest()
-      req.open(method, url, true)
-      for (const header in headers) {
-        req.setRequestHeader(header, headers[header])
-      }
-      req.responseType = 'arraybuffer'
-      req.onload = event => {
-        const resp = req.response
-        if (resp) {
-          resolve(resp)
-        }
-      }
-      req.onerror = err => reject(err)
-      req.send(data)
-    })
-
-  let result = Buffer.alloc(0)
-  const opts: FetchOptions = { method, headers }
-  if (data) Object.assign(opts, { body: data })
-  // Use the modern API if in node or any enviroment which supports it
-  if (
-    typeof window === 'undefined' ||
-    (window.Response && window.Response.prototype.arrayBuffer)
-  ) {
-    result = await fetch(url, opts)
-    result = await result.arrayBuffer()
-  } else if (window && window.XMLHttpRequest) {
-    result = await legacyBufferFetch()
-  }
-  return Buffer.from(result)
-}
 
 const getSpendTargets = (outputs: Array<any>, network: string, currencyCode: string) => {
   let nativeAmount = 0
@@ -105,42 +52,13 @@ const getBitPayPayment = async (
   return edgePaymentProtocolInfo
 }
 
-export const parsePayment = (
-  paymentBuffer: Buffer,
-  network: string,
-  currencyCode: string
-): EdgePaymentProtocolInfo => {
-  const paymentRequest = bip70.PaymentRequest.fromRaw(paymentBuffer)
-  const { paymentDetails } = paymentRequest
-  const { paymentUrl = '', memo = '', outputs = [] } = paymentDetails
-  const merchantData =
-    paymentDetails.getData('json') || paymentDetails.merchantData
-  const domain = parse(paymentUrl, {}).hostname
-  const { nativeAmount, spendTargets } = getSpendTargets(outputs, network, currencyCode)
-
-  const edgePaymentProtocolInfo: EdgePaymentProtocolInfo = {
-    nativeAmount: `${nativeAmount}`,
-    merchant: merchantData || '',
-    memo,
-    domain,
-    spendTargets,
-    paymentUrl
-  }
-
-  return edgePaymentProtocolInfo
-}
-
 export async function getPaymentDetails (
   paymentProtocolURL: string,
   network: string,
   currencyCode: string,
   fetch: any
 ): Promise<EdgePaymentProtocolInfo> {
-  const domain = parse(paymentProtocolURL, {}).hostname
-  if (domain === 'bitpay.com') return getBitPayPayment(paymentProtocolURL, network, fetch)
-  const headers = { Accept: `application/${network}-paymentrequest` }
-  const buf = await fetchBuffer(fetch, paymentProtocolURL, 'GET', headers, null)
-  return parsePayment(buf, network, currencyCode)
+  return getBitPayPayment(paymentProtocolURL, network, fetch)
 }
 
 export function createPayment (
@@ -149,18 +67,7 @@ export function createPayment (
   tx: string,
   currencyCode: string
 ): any {
-  if (paymentDetails.domain === 'bitpay.com') {
-    return { currency: currencyCode, transactions: [tx] }
-  } else {
-    const refundOutput = primitives.Output.fromOptions({ value: paymentDetails.nativeAmount, address: refundAddress })
-    const txObj = primitives.TX.fromRaw(tx, 'hex')
-    return bip70.Payment.fromOptions({
-      transactions: [txObj],
-      refundTo: [refundOutput],
-      memo: paymentDetails.memo,
-      merchantData: paymentDetails.merchant
-    }).toRaw()
-  }
+  return { currency: currencyCode, transactions: [tx] }
 }
 
 export async function sendPayment (
@@ -169,23 +76,22 @@ export async function sendPayment (
   paymentUrl: string,
   payment: any
 ): Promise<any> {
-  const domain = parse(paymentUrl, {}).hostname
-  if (domain === 'bitpay.com') {
-    const headers = { 'Content-Type': 'application/payment' }
-    const result = await fetch(paymentUrl, { method: 'POST', headers, body: JSON.stringify(payment) })
-    if (parseInt(result.status) !== 200) {
-      const error = await result.text()
-      throw new Error(error)
+  const headers = { 'Content-Type': 'application/payment' }
+  if (global.androidFetch) {
+    try {
+      const result = await global.androidFetch(paymentUrl, { method: 'POST', headers, body: JSON.stringify(payment) })
+      const paymentACK = JSON.parse(result)
+      return paymentACK
+    } catch (e) {
+      console.log(e)
+      throw e
     }
-    const paymentACK = await result.json()
-    return paymentACK
-  } else {
-    const headers = {
-      'Content-Type': `application/${network}-payment`,
-      Accept: `application/${network}-paymentack`
-    }
-    const buf = await fetchBuffer(fetch, paymentUrl, 'POST', headers, payment)
-    const paymentACK = bip70.PaymentACK.fromRaw(buf)
-    return paymentACK
   }
+  const result = await fetch(paymentUrl, { method: 'POST', headers, body: JSON.stringify(payment) })
+  if (parseInt(result.status) !== 200) {
+    const error = await result.text()
+    throw new Error(error)
+  }
+  const paymentACK = await result.json()
+  return paymentACK
 }
