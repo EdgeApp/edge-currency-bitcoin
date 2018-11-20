98776db1384ef47d06160011495e5f6d7845c01f
adyshimony <ady.shimony@gmail.com>
2018-11-20 02:03:34 +0200

    Add bitcionsv TODO: check cashAddress: 'bitcoinsv' format

--- src/info/bitcoincash.js
+++ src/info/bitcoincash.js
@@ -8,6 +8,7 @@ const bcoinInfo: BcoinCurrencyInfo = {
   type: 'bitcoincash',
   magic: 0xd9b4bef9,
   formats: ['bip44', 'bip32'],
+  forks: ['bitcoincashsv'],
   keyPrefix: {
     privkey: 0x80,
     xpubkey: 0x0488b21e,
