8351de6fd55eee21f58a59af18b72e8b7720d5c7
thehobbit85 <thehobbit85@gmail.com>
2018-11-27 03:38:48 -0800

    Ignore spentTargets without and address

--- src/engine/keyManager.js
+++ src/engine/keyManager.js
@@ -218,7 +218,7 @@ export class KeyManager {
           address = this.getNextAvailable(keyRing.children)
         }
       }
-      standardOutputs.push({ address, value: output.value })
+      if (address) standardOutputs.push({ address, value: output.value })
     }
     return createTX({
       ...rest,
