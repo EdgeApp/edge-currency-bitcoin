a49bb2b7a851011b338d349ca6d889210419f6d0
adyshimony <ady.shimony@gmail.com>
2018-12-26 19:44:01 +0200

    Refactor fixtures

--- test/utils/coinUtils/coinUtils.js
+++ test/utils/coinUtils/coinUtils.js
@@ -21,20 +21,18 @@ import * as Factories from '../../../src/index'
 import fixtures from './fixtures.json'
 
 for (const fixture of fixtures) {
-  const { network, pluginName, coinType, format } = fixture
-
   describe('getReceiveAddresses', function () {
-    fixture['addressesRawTxs'].forEach(tx => {
+    fixture['addressesRawTxs'].forEach(([network, raw, expectedAddress]) => {
       it('Test getReceiveAddresses', async function () {
-        const bcoinTx: any = primitives.TX.fromRaw(tx.raw, 'hex')
+        const bcoinTx: any = primitives.TX.fromRaw(raw, 'hex')
         const address = await getReceiveAddresses(bcoinTx, network)
-        assert.equal(address[0], tx.expectedAddress)
+        assert.equal(address[0], expectedAddress)
       })
     })
   })
 
   describe('isCompressed', function () {
-    fixture['compressedkeys'].forEach(key => {
+    fixture['compressedkeys'].forEach(([network, key]) => {
       it('Check if key is compressed', function () {
         const compressed: boolean = isCompressed(Buffer.from(key, 'hex'))
         assert(compressed, 'Key is not compressed')
@@ -43,7 +41,7 @@ for (const fixture of fixtures) {
   })
 
   describe('isCompressed', function () {
-    fixture['unCompressedkeys'].forEach(key => {
+    fixture['unCompressedkeys'].forEach(([network, key]) => {
       it('Check if key is un compressed', function () {
         const unCompressed: boolean = isCompressed(Buffer.from(key, 'hex'))
         assert(!unCompressed, 'Key is compressed')
@@ -52,7 +50,7 @@ for (const fixture of fixtures) {
   })
 
   describe('verifyWIF', function () {
-    fixture['WIF'].forEach(wifKey => {
+    fixture['WIF'].forEach(([network, wifKey]) => {
       it('Verify WIF format', function () {
         const isWif: boolean = verifyWIF(wifKey, network)
         assert(isWif, 'key is not wif')
@@ -61,46 +59,46 @@ for (const fixture of fixtures) {
   })
 
   describe('addressToScriptHash', function () {
-    fixture['scriptHashAddresses'].forEach(addressToScript => {
+    fixture['scriptHashAddresses'].forEach(([network, address, expectedScriptHash]) => {
       it('Test addressToScriptHash', async function () {
         const scriptHash = await addressToScriptHash(
-          addressToScript.address,
+          address,
           network
         )
-        assert.equal(scriptHash, addressToScript.expectedScriptHash)
+        assert.equal(scriptHash, expectedScriptHash)
       })
     })
   })
 
   describe('verifyUriProtocol', function () {
-    fixture['URIs'].forEach(uri => {
+    fixture['URIs'].forEach(([network, protocol, pluginName]) => {
       it('Verify URI format', function () {
-        const protocol: boolean = verifyUriProtocol(
-          uri.protocol,
+        const protocolRes: boolean = verifyUriProtocol(
+          protocol,
           network,
           pluginName
         )
-        assert(protocol, 'protocol not verified')
+        assert(protocolRes, 'protocol not verified')
       })
     })
   })
 
   describe('getPrivateFromSeed', function () {
-    fixture['seedToXpriv'].forEach(key => {
+    fixture['seedToXpriv'].forEach(([network, seed, xpriv]) => {
       it('Verify Private From Seed', async function () {
-        const resKey = await getPrivateFromSeed(key.seed, network)
-        assert.equal(resKey.xprivkey(), key.xpriv)
+        const resKey = await getPrivateFromSeed(seed, network)
+        assert.equal(resKey.xprivkey(), xpriv)
       })
     })
   })
 
   describe('keysFromEntropy', function () {
-    fixture['entropyKeys'].forEach(key => {
-      const randomBuffer = Buffer.from(key.entropy, 'hex')
+    fixture['entropyKeys'].forEach(([network, coinType, format, entropy, seed]) => {
+      const randomBuffer = Buffer.from(entropy, 'hex')
       it('Test keysFromEntropy', function () {
         const opts = { format, coinType }
         const res = keysFromEntropy(randomBuffer, network, opts)
-        assert.equal(res[network + 'Key'], key.seed)
+        assert.equal(res[network + 'Key'], seed)
         assert.equal(res.format, format)
         assert.equal(res.coinType, coinType)
       })
@@ -108,7 +106,7 @@ for (const fixture of fixtures) {
   })
 
   describe('createTx', function () {
-    fixture['createTx'].forEach(txData => {
+    fixture['createTx'].forEach(([network, txData]) => {
       const utxos = []
       txData.utxos.forEach(utxo => {
         utxos.push({
@@ -150,23 +148,23 @@ for (const fixture of fixtures) {
   })
 
   describe('parseTransaction', function () {
-    fixture['parseTx'].forEach(txData => {
+    fixture['parseTx'].forEach(([rawTx, expected]) => {
       it('Matches a known transaction', function () {
-        const parsedData = parseTransaction(txData.rawTx)
+        const parsedData = parseTransaction(rawTx)
         for (let index = 0; index < parsedData.inputs.length; index++) {
           expect(parsedData.inputs[index].prevout.rhash()).to.equal(
-            txData.expected.inputs[index].txid
+            expected.inputs[index].txid
           )
           expect(parsedData.inputs[index].prevout.index).to.equal(
-            txData.expected.inputs[index].index
+            expected.inputs[index].index
           )
         }
         for (let index = 0; index < parsedData.outputs.length; index++) {
           expect(parsedData.outputs[index].scriptHash).to.equal(
-            txData.expected.outputs[index].scriptHash
+            expected.outputs[index].scriptHash
           )
           expect(parsedData.outputs[index].value).to.equal(
-            txData.expected.outputs[index].value
+            expected.outputs[index].value
           )
         }
       })
