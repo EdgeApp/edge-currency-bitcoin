b717375cb0618e842591dd19ddcef60bbdfd8937
eliran zach <thehobbit85@gmail.com>
2019-02-10 19:42:59 +0200

    Add 'privateFromWIF' and 'keyPairFromWIF'

--- perian/perian-core/src/commons/keyPair.js
+++ perian/perian-core/src/commons/keyPair.js
@@ -3,6 +3,7 @@
 import { Crypto } from '@perian/core-utils'
 
 import type { KeyPair } from '../../types/keyPair.js'
+import { networks } from './network.js'
 
 export const fromHex = (keyHex: string): KeyPair<string> => {
   if (keyHex.length !== 66 && keyHex.length !== 132) {
@@ -29,10 +30,40 @@ export const fromHex = (keyHex: string): KeyPair<string> => {
   }
 }
 
+export const privateFromWIF = (
+  wif: string,
+  network: string = 'main'
+): { privateKey: string, compress: boolean } => {
+  const { serializers, keyPrefix } = networks[network]
+  const keyHex = serializers.wif.decode(wif)
+  if (parseInt(keyHex.slice(0, 2), 16) !== keyPrefix.privkey) {
+    throw new Error(
+      `Unknown key prefix ${keyHex.slice(0, 2)} for network ${network}`
+    )
+  }
+  const privateKey = keyHex.slice(2, 66)
+  let compress = false
+  if (keyHex.length >= 68) {
+    if (parseInt(keyHex.slice(66, 68), 16) !== 1) {
+      throw new Error(`Unknown compression flag ${keyHex.slice(66, 68)}`)
+    }
+    compress = true
+  }
+  return { privateKey, compress }
+}
+
+export const keyPairFromWIF = async (
+  wif: string,
+  network: string = 'main'
+): Promise<KeyPair<string>> => {
+  const { privateKey, compress } = privateFromWIF(wif, network)
+  const publicKey = await Crypto.publicKeyCreate(privateKey, compress)
+  return { privateKey, publicKey }
+}
+
 export const toHex = (keyPair: KeyPair<string>): string => {
   const publicKey = keyPair.publicKey || ''
   const privateKey = keyPair.privateKey ? `00${keyPair.privateKey}` : ''
-
   return `${privateKey}${publicKey}`
 }
 
