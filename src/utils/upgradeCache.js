05aa0536e9ac7c2dfd482772de3437903f4a33b1
Matthew <matthew@edge.app>
2020-02-12 15:43:40 -0800

    upgrade cache to v3

--- src/utils/upgradeCache.js
+++ src/utils/upgradeCache.js
@@ -9,7 +9,9 @@ import { toNewFormat } from '../utils/addressFormat.js'
 import { parsePath } from '../utils/coinUtils.js'
 import { formatSelector } from '../utils/formatSelector.js'
 
-const version2 = async (
+const versionNumber = 3
+
+const version3 = async (
   options: EdgeCurrencyEngineOptions,
   walletInfo: EdgeWalletInfo,
   engineInfo: EngineCurrencyInfo
@@ -35,15 +37,28 @@ const version2 = async (
         const addressObj = cacheJson.addresses[addressHex]
         const [branch, index] = parsePath(addressObj.path, masterPath)
         const pubKey = await fSelector.deriveHdKey(masterKeys.pubKey, branch)
-        const { address } = await fSelector.deriveAddress(pubKey, index)
-        addressObj.displayAddress = toNewFormat(address, network)
+        let tempAddress
+        if (branch === 146473132) {
+          const { address } = await fSelector.deriveScriptAddress(
+            pubKey,
+            index,
+            branch
+          )
+          tempAddress = address
+        } else {
+          const { address } = await fSelector.deriveAddress(pubKey, index)
+          tempAddress = address
+        }
+        addressObj.displayAddress = toNewFormat(tempAddress, network)
       }
       const stringifiedAddresses = JSON.stringify(cacheJson)
       await localDisklet.setText('addresses.json', stringifiedAddresses)
-      await localDisklet.setText('version.txt', '2')
-    } catch (e) {}
+    } catch (e) {
+    } finally {
+      await localDisklet.setText('version.txt', `${versionNumber}`)
+    }
   } else {
-    await localDisklet.setText('version.txt', '2')
+    await localDisklet.setText('version.txt', `${versionNumber}`)
   }
 }
 
@@ -54,8 +69,9 @@ export const checkCacheVersion = async (
 ) => {
   try {
     // should fail in case of no version file (version 1)
-    await options.walletLocalDisklet.getText('version.txt')
+    const version = await options.walletLocalDisklet.getText('version.txt')
+    if (parseInt(version) < versionNumber) throw new Error()
   } catch (e) {
-    await version2(options, walletInfo, engineInfo)
+    await version3(options, walletInfo, engineInfo)
   }
 }
