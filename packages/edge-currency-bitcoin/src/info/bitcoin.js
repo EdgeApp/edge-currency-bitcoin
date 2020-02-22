f668a6c0de33ecc9bb089070eaf759f2f8ad1066
Shawn Cook <sfncook@gmail.com>
2020-02-21 17:46:34 -0800

    Removed ssl servers from default list

--- packages/edge-currency-bitcoin/src/info/bitcoin.js
+++ packages/edge-currency-bitcoin/src/info/bitcoin.js
@@ -32,22 +32,13 @@ const currencyInfo: $Shape<EdgeCurrencyInfo> = {
   // Configuration options:
   defaultSettings: {
     electrumServers: [
-      'electrums://electrum-bc-az-eusa.airbitz.co:50002',
       'electrum://electrum-bc-az-eusa.airbitz.co:50001',
       'electrum://electrum.hsmiths.com:8080',
-      'electrums://E-X.not.fyi:50002',
-      'electrums://node.arihanc.com:50002',
       'electrum://node.arihanc.com:50001',
-      'electrums://electrum.petrkr.net:50002',
       'electrum://electrum.petrkr.net:50001',
-      'electrums://electrum2.everynothing.net:50002',
       'electrum://electrum2.everynothing.net:50001',
-      'electrums://lith.strangled.net:50002',
-      'electrums://s4.noip.pl:50104',
       'electrum://currentlane.lovebitco.in:50001',
-      'electrums://electrum.hsmiths.com:50002',
       'electrum://electrum.hsmiths.com:50001',
-      'electrums://electrumx.westeurope.cloudapp.azure.com:50002',
       'electrum://electrumx.westeurope.cloudapp.azure.com:50001'
     ]
   },
