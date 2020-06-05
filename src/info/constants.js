0d318dc5c3e7b13008c863515f4604ce5c28dcca
William Swanson <swansontec@gmail.com>
2020-06-05 16:32:09 -0700

    Upgrade to the new info server URL

--- src/info/constants.js
+++ src/info/constants.js
@@ -1,7 +1,7 @@
 // @flow
 
 export const imageServerUrl = 'https://developer.airbitz.co/content'
-export const InfoServer = 'https://info1.edgesecure.co:8444/v1'
+export const InfoServer = 'https://info1.edge.app/v1'
 
 export const FixCurrencyCode = (currencyCode: string): string => {
   switch (currencyCode) {
