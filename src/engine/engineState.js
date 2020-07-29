6ac2663fd851886b552c6e02287c06659a269078
Hakob Kanoyan <hakob91@gmail.com>
2020-07-29 11:39:00 +0400

    create interfaces

--- src/engine/engineState.js
+++ src/engine/engineState.js
@@ -34,6 +34,7 @@ import {
 } from '../utils/coinUtils.js'
 import { pushUpdate, removeIdFromQueue } from '../utils/updateQueue.js'
 import { type EngineCurrencyInfo } from './currencyEngine.js'
+import { EngineStateExtension } from './engineStateExtension'
 
 export type UtxoInfo = {
   txid: string, // tx_hash from Stratum
@@ -97,6 +98,7 @@ export interface EngineStateOptions {
   pluginState: PluginState;
   walletId?: string;
   engineInfo: EngineCurrencyInfo;
+  engineStateExtensions: ?EngineStateExtension;
 }
 
 function nop() {}
@@ -346,6 +348,10 @@ export class EngineState extends EventEmitter {
   }
 
   getBalance(options: any): string {
+    if (this.stateExtension && this.stateExtension.getBalance) {
+      return this.stateExtension.getBalance(options)
+    }
+
     return Object.keys(this.addressInfos)
       .reduce((total, scriptHash) => {
         const { balance } = this.addressInfos[scriptHash]
@@ -378,6 +384,11 @@ export class EngineState extends EventEmitter {
   }
 
   dumpData(): any {
+    let dump = {}
+    if (this.stateExtension && this.stateExtension.dumpData) {
+      dump = this.stateExtension.dumpData()
+    }
+
     return {
       'engineState.addressCache': this.addressCache,
       'engineState.addressInfos': this.addressInfos,
@@ -389,7 +400,8 @@ export class EngineState extends EventEmitter {
       'engineState.serverStates': this.serverStates,
       'engineState.fetchingTxs': this.fetchingTxs,
       'engineState.missingTxs': this.missingTxs,
-      'engineState.fetchingHeader': this.fetchingHeaders
+      'engineState.fetchingHeader': this.fetchingHeaders,
+      ...dump
     }
   }
 
@@ -418,6 +430,7 @@ export class EngineState extends EventEmitter {
   progressRatio: number
   txCacheInitSize: number
   serverList: Array<string>
+  stateExtension: ?EngineStateExtension
 
   constructor(options: EngineStateOptions) {
     super()
@@ -443,6 +456,7 @@ export class EngineState extends EventEmitter {
     this.pluginState = options.pluginState
     this.serverList = []
     this.engineInfo = options.engineInfo
+    this.stateExtension = options.engineStateExtensions
     this.log = options.log
     const {
       onBalanceChanged = nop,
@@ -848,6 +862,10 @@ export class EngineState extends EventEmitter {
         )
       }
     }
+
+    if (this.stateExtension && this.stateExtension.pickNextTask) {
+      return this.stateExtension.pickNextTask(uri, stratumVersion)
+    }
   }
 
   async load() {
@@ -914,6 +932,10 @@ export class EngineState extends EventEmitter {
       this.addressInfos = {}
       this.txHeightCache = {}
       this.log(`Failed to load address cache: ${e}`)
+    }
+
+    if (this.stateExtension) {
+      this.stateExtension.load(this)
     }
 
     return this
